a73e613942e7023244dfd4cab91d8607c2466a80
Caio Anderson <caioandersongoes@gmail.com>
2025-11-13 11:59:04 -0300

    Rotas removidas por ter desfeito o uso do back-end

--- escola-frontend/src/routes/routes.tsx
+++ escola-frontend/src/routes/routes.tsx
@@ -1,26 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import BaseLayout from "@/components/global/BaseLayout";
+// import BaseLayout from "@/components/global/BaseLayout";
 import ClientLayout from "@/components/global/ClientLayout";
-import AdminLayout from "@/components/global/AdminLayout";
+// import AdminLayout from "@/components/global/AdminLayout";
 
-import Landing from "@/pages/Landing/Landing";
-import Login from "@/pages/Login/Login";
+// import Landing from "@/pages/Landing/Landing";
+// import Login from "@/pages/Login/Login";
 
 import Home from "@/pages/ClientPages/Home/Home";
 import Sobre from "@/pages/ClientPages/Sobre/Sobre";
-import Event from "@/pages/ClientPages/Event/Events";
+// import Event from "@/pages/ClientPages/Event/Events";
 import FAQ from "@/pages/ClientPages/FAQ/FAQ";
 import Contact from "@/pages/ClientPages/Contact/Contact";
 
-import AdminHome from "@/pages/AdminPages/Home/AdminHome";
-import AdminPromotion from "@/pages/AdminPages/Promotion/AdminPromotion";
-import AdminFeedback from "@/pages/AdminPages/Feedback/AdminFeedback";
-import AdminEvent from "@/pages/AdminPages/Event/AdminEvent";
-import AdminFAQ from "@/pages/AdminPages/FAQ/AdminFAQ";
+// import AdminHome from "@/pages/AdminPages/Home/AdminHome";
+// import AdminPromotion from "@/pages/AdminPages/Promotion/AdminPromotion";
+// import AdminFeedback from "@/pages/AdminPages/Feedback/AdminFeedback";
+// import AdminEvent from "@/pages/AdminPages/Event/AdminEvent";
+// import AdminFAQ from "@/pages/AdminPages/FAQ/AdminFAQ";
 
 import NotFoundPage from "@/pages/NotFound/NotFoundPage";
-import AdminRoutes from "./AdminRoutes";
+// import AdminRoutes from "./AdminRoutes";
 
 export const router = createBrowserRouter([
   {
@@ -31,22 +31,22 @@ export const router = createBrowserRouter([
       </ClientLayout>
     ),
   },
-  {
-    path: "/login",
-    element: (
-      <BaseLayout>
-        <Login />
-      </BaseLayout>
-    ),
-  },
-  {
-    path: "/painel",
-    element: (
-      <BaseLayout>
-        <Landing />
-      </BaseLayout>
-    ),
-  },
+  // {
+  //   path: "/login",
+  //   element: (
+  //     <BaseLayout>
+  //       <Login />
+  //     </BaseLayout>
+  //   ),
+  // },
+  // {
+  //   path: "/painel",
+  //   element: (
+  //     <BaseLayout>
+  //       <Landing />
+  //     </BaseLayout>
+  //   ),
+  // },
   {
     path: "/sobre",
     element: (
@@ -55,14 +55,14 @@ export const router = createBrowserRouter([
       </ClientLayout>
     ),
   },
-  {
-    path: "/eventos",
-    element: (
-      <ClientLayout>
-        <Event />
-      </ClientLayout>
-    ),
-  },
+  // {
+  //   path: "/eventos",
+  //   element: (
+  //     <ClientLayout>
+  //       <Event />
+  //     </ClientLayout>
+  //   ),
+  // },
   {
     path: "/perguntas-frequentes",
     element: (
@@ -80,52 +80,52 @@ export const router = createBrowserRouter([
     ),
   },
 
-  {
-    path: "/admin",
-    element: <AdminRoutes />,
-    children: [
-      {
-        path: "inicio",
-        element: (
-          <AdminLayout>
-            <AdminHome />
-          </AdminLayout>
-        ),
-      },
-      {
-        path: "promocao",
-        element: (
-          <AdminLayout>
-            <AdminPromotion />
-          </AdminLayout>
-        ),
-      },
-      {
-        path: "feedback",
-        element: (
-          <AdminLayout>
-            <AdminFeedback />
-          </AdminLayout>
-        ),
-      },
-      {
-        path: "eventos",
-        element: (
-          <AdminLayout>
-            <AdminEvent />
-          </AdminLayout>
-        ),
-      },
-      {
-        path: "perguntas-frequentes",
-        element: (
-          <AdminLayout>
-            <AdminFAQ />
-          </AdminLayout>
-        ),
-      },
-    ],
-  },
+  // {
+  //   path: "/admin",
+  //   element: <AdminRoutes />,
+  //   children: [
+  //     {
+  //       path: "inicio",
+  //       element: (
+  //         <AdminLayout>
+  //           <AdminHome />
+  //         </AdminLayout>
+  //       ),
+  //     },
+  //     {
+  //       path: "promocao",
+  //       element: (
+  //         <AdminLayout>
+  //           <AdminPromotion />
+  //         </AdminLayout>
+  //       ),
+  //     },
+  //     {
+  //       path: "feedback",
+  //       element: (
+  //         <AdminLayout>
+  //           <AdminFeedback />
+  //         </AdminLayout>
+  //       ),
+  //     },
+  //     {
+  //       path: "eventos",
+  //       element: (
+  //         <AdminLayout>
+  //           <AdminEvent />
+  //         </AdminLayout>
+  //       ),
+  //     },
+  //     {
+  //       path: "perguntas-frequentes",
+  //       element: (
+  //         <AdminLayout>
+  //           <AdminFAQ />
+  //         </AdminLayout>
+  //       ),
+  //     },
+  //   ],
+  // },
 
   {
     path: "*",
