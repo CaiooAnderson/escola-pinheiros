9fc54c825dd80de9ed15ddd4c375248b78e203f3
Caio Anderson <caioandersongoes@gmail.com>
2025-11-18 08:41:22 -0300

    Adicionado componente para carousel de entrada

--- escola-frontend/src/pages/ClientPages/Home/Home.tsx
+++ escola-frontend/src/pages/ClientPages/Home/Home.tsx
@@ -2,11 +2,13 @@ import Hero from "../Home/components/Hero";
 import About from "../Home/components/About";
 import Promotion from "../Home/components/Promotion";
 import Feedback from "../Home/components/Feedback";
+// import IntroCarousel from "./components/IntroCarousel";
 import FloatButton from "@/components/global/FloatButton";
 
 export default function Home() {
   return (
     <div className="w-full">
+      {/* <IntroCarousel /> */}
       <Hero />
       <About />
       <Promotion />
